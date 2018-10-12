cdcd42f89e6f42f7cef7b3d6214f22c99ee34547
abhishekavi4602 <abhishekavi4602@gmail.com>
2018-10-13 00:15:26 +0530

    First Commit

---
+++ app/src/main/java/com/codingblocks/notes/Note.java
@@ -0,0 +1,43 @@
+package com.codingblocks.notes;
+
+public class Note {
+
+    private String title, time;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private Integer id;
+
+    public Note(String title, String time, Integer id) {
+        this.title = title;
+        this.time = time;
+        this.id=id;
+    }
+
+    public Note(String title, String time ) {
+        this.title = title;
+        this.time = time;
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getTime() {
+        return time;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
